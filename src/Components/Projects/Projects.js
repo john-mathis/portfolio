adcc35676d2e07a4e9aa69b5aaf2c0a1a47e8f34
John Mathis <johnmathis98@gmail.com>
2022-10-02 19:39:11 -0400

    added projects component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "..//Projects/Projects.css";
+import CAT from "..//..//assets/cat.webp";
 
 const Projects = () => {
   return (
@@ -7,6 +8,33 @@ const Projects = () => {
       <div className="projects-container">
         <div className="max-container-width">
           <h3 className="section-title">Projects</h3>
+
+          <div className="container">
+            <div className="project-card">
+              <p>REST Countries API</p>
+              <div className="card-img">
+                <div className="card-overlay">
+                  <img className="project-img" src={CAT} />
+                </div>
+              </div>
+            </div>
+
+            <div className="project-card">
+              <img className="project-img" src={CAT} />
+            </div>
+
+            <div className="project-card">
+              <img className="project-img" src={CAT} />
+            </div>
+
+            <div className="project-card">
+              <img className="project-img" src={CAT} />
+            </div>
+
+            <div className="project-card">
+              <img className="project-img" src={CAT} />
+            </div>
+          </div>
         </div>
       </div>
     </>
