9bb2be8a752f8d4a9cd612ac5fcae8372f78d8d4
John Mathis <johnmathis98@gmail.com>
2022-11-07 17:26:11 -0500

    lengthed homepage. updated project picture

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "..//Projects/Projects.css";
 import Country from "..//..//assets/country-project.png";
+import Planets from "..//..//assets/planets-project.png";
 import Portfolio from "..//..//assets/portfolio-project.png";
 import Groupchat from "..//..//assets/groupchat-landing-page.png";
 import Mastercraft from "..//..//assets/mastercraft-project.png";
@@ -14,6 +15,13 @@ const Projects = () => {
       github: "https://github.com/mackmathis/Country-API",
       live: "https://mackmathis.github.io/Country-API/",
       img: Country,
+    },
+
+    {
+      name: "Planets",
+      github: "https://github.com/mackmathis/react-planets",
+      live: "https://mackmathis.github.io/react-planets/",
+      img: Planets,
     },
 
     {
@@ -63,34 +71,6 @@ const Projects = () => {
 
   return (
     <>
-      {/* <div className="projects-container" id="projects">
-        <div className="max-container-width">
-          <h3 className="section-title">Projects</h3>
-
-          <div className="container">
-            <div className="project-card">
-              <p className="project-name">REST Countries API</p>
-              <img className="project-img" src={Country} />
-              <div className="card-overlay">
-                <a
-                  target="blank"
-                  href="https://github.com/mackmathis/Country-API"
-                >
-                  <FiGithub className="project-icons" />
-                </a>
-
-                <a
-                  target="blank"
-                  href="https://mackmathis.github.io/Country-API/"
-                >
-                  <CgExport className="project-icons" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
-
       <div className="projects-container" id="projects">
         <div className="max-container-width">
           <h3 className="section-title">Projects</h3>
