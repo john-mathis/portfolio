15f8f5d0f3fb463689db1e1226b072d18b01e74b
John Mathis <johnmathis98@gmail.com>
2023-04-10 18:48:44 -0400

    deleted comments and updated nba app links

--- src/Components/ProjectData/ProjectData.js
+++ src/Components/ProjectData/ProjectData.js
@@ -2,26 +2,11 @@ import Planets from "..//..//assets/planets-project.png";
 import NBA from "..//..//assets/nba-project.png";
 import Barbershop from "..//..//assets/barbershop-project.jpg";
 
-// name: "React Dev Portfolio",
-// github: "https://github.com/john-mathis/React-dev-portfolio",
-// live: "https://john-mathis.github.io/React-dev-portfolio/",
-// img: ReactDev,
-
-// name: "Rock Paper Scissors",
-//     github: "https://github.com/john-mathis/rock-paper-scissors",
-//     live: "https://john-mathis.github.io/rock-paper-scissors/",
-//     img: Mastercraft,
-
-// name: "Rest Countries API",
-// github: "https://github.com/john-mathis/Country-API",
-// live: "https://john-mathis.github.io/Country-API/",
-// img: Country,
-
 export const ProjectData = [
   {
     name: "NBA STATS ",
-    github: "https://github.com/john-mathis/portfolio",
-    live: "https://john-mathis.github.io/portfolio/",
+    github: "https://github.com/john-mathis/NBA",
+    live: "https://nba-fawn.vercel.app/",
     img: NBA,
     description: "Created with MongoDB, Express, ReactJS, and NodeJS. ",
   },
