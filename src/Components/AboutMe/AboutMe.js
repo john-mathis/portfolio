c077dd0d919d4487b4870abf7f70a73b4d7e18bd
john-mathis <johnmathis98@gmail.com>
2025-07-22 15:46:59 -0400

    updated about me section

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,44 +1,45 @@
+import Resume from "../../assets/John_Mathis_Resume.pdf";
 import John from "../../assets/john.jpg";
-
 
 const AboutMe = () => {
   return (
-    <>
-      <div className="bg-[var(--dark-grey)] min-h-140 lg: min-h-260 py-12">
-        <div className="max-container-width flex flex-col lg:flex-row">
-          <div className="flex m-auto	lg:order-2">
-            <img
-              alt="john mathis using a macbook"
-              src={John}
-              className="max-w-[15rem]
-              max-h-[15rem]mt-8 rounded-lg  sm:max-w-xs sm:max-h-100              "
-            ></img>
+    <section className="min-h-140 lg: min-h-260 py-12 py-18 px-4">
+      <div className="max-container-width mx-auto flex flex-col lg:flex-row items-center gap-12">
+        {/* Left: Image */}
+        <div className="flex-1 max-w-xs lg:max-w-sm">
+          <img
+            src={John}
+            alt="John Mathis on laptop"
+            className="rounded-xl w-full object-cover grayscale hover:grayscale-0 transition duration-300"
+          />
+        </div>
+        {/* Right: Text */}
+        <div className=" m-auto text-center lg:text-left">
+          <h1 className="text-5xl md:text-7xl font-semibold text-white mb-3 ">
+            Hi, I’m John.
+          </h1>
+          <h2 className="text-2xl md:text-4xl leading-relaxed mb-8 max-w-xl">
+            I’m a software developer.
+          </h2>
+        
+          <div className="flex m-auto space-x-4 mt-4 justify-center items-center lg:justify-start">
+            <a className="inline-block bg-[white] border text-black text-sm font-medium px-6 py-3 rounded-md hover:scale-105 transition-transform" href="https://github.com/johnmathis" target="_blank" rel="noopener noreferrer"> Github
+            </a>
+            <a
+            className="inline-block bg-[var(--red)] text-white text-sm font-medium px-6 py-3 rounded-md hover:scale-105 transition-transform cursor-pointer"
+          >
+            Download CV
+          </a>
+          <a
+            className="inline-block bg-[white] border text-black text-sm font-medium px-6 py-3 rounded-md hover:scale-105 transition-transform" href="https://github.com/johnmathis" target="_blank" rel="noopener noreferrer">
+            Linkedin
+          </a>
           </div>
-          <div className="text-center mb-8 lg:text-left lg:mr-8 lg:flex lg:flex-col lg:order-1 lg:mb-0 lg:justify-evenly ">
-            <h3 className="text-3xl md:text-4xl my-8 underline underline-offset-[var(--text-underline-spacing)] font-normal mb-4">
-              Hi, I'm John.
-            </h3>
-            <p className="text-base w-150 h-fit mb-8 md:text-lg md:min-h-48 md:w-84 lg:text-xl lg:max-w-[90%] xl:max-w-[85%]	">
-              As a passionate software developer and perpetual learner, I hold a
-              deep appreciation for the creation of clean, efficient, and
-              cutting-edge solutions. I am driven by the potential of software
-              to make a positive impact on individuals and society at large. The
-              process of overcoming challenges and acquiring new skills to solve
-              intricate problems brings me immense satisfaction in my
-              development journey.
-            </p>
 
-            {/* <a
-              href={Resume}
-              download="John Mathis Resume"
-              className="hover:scale-125 no-underline bg-[var(--red)] w-32 text-center text-white	p-3	relative"
-            >
-              Download CV
-            </a> */}
-          </div>
+
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
